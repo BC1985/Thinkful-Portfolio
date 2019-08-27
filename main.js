dad59af2f2f82798ce0a7f6e74bf5511aa43b80e
Ben Cohen <benjcohen1985@gmail.com>
2019-08-27 10:31:26 -0400

    Remove comments

--- main.js
+++ main.js
@@ -1,30 +1,23 @@
-'use strict'
+"use strict";
 
-//on click the nav is displayed
-let menu = ('#js-menu')
-let toggle = '#js-navbar-toggle'
+let menu = "#js-menu";
+let toggle = "#js-navbar-toggle";
 
 function toggleMenu() {
-
-    $(toggle).click(function (event) {
-        $(menu).toggleClass('active');
-    });
+  $(toggle).click(function() {
+    $(menu).toggleClass("active");
+  });
 }
 
-//close the navbar when link is clicked
 function closeNav() {
-    $(menu).on('click', 'li', function () {
-        $(menu).removeClass('active');
-
-    });
-
+  $(menu).on("click", "li", function() {
+    $(menu).removeClass("active");
+  });
 }
 
 function renderPage() {
-
-    toggleMenu();
-    closeNav();
-
+  toggleMenu();
+  closeNav();
 }
 
-$(renderPage)
+$(renderPage);
